09a8a00cde3bf7052b808827e4d487b86466f754
viim003 <viim003@viim003ui-MacBookPro.local>
2025-07-09 02:01:50 +0900

    s3 수정

--- src/main/java/sw_workbook/spring/web/controller/MemberViewController.java
+++ src/main/java/sw_workbook/spring/web/controller/MemberViewController.java
@@ -1,14 +1,13 @@
 package sw_workbook.spring.web.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import sw_workbook.spring.apiPayload.ApiResponse;
 import sw_workbook.spring.service.memberservice.MemberCommandService;
 import sw_workbook.spring.web.dto.MemberRequestDTO;
 
@@ -54,6 +53,10 @@ public class MemberViewController {
             return "signup";
         }
     }
+
+//    @PostMapping("/{storeid}/review")
+//    public ApiResponse<StoreReponseDTO.CreateReviewResultDTO> createReview(@RequestBody @Valid storeRequestDTO.ReviewDTO request,
+//                                                                           @Exist)
 
 
 }
